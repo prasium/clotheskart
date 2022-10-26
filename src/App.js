8ea9973f3dd042db031002060993ee8374355292
Prasun Verma <prasunverma12@gmail.com>
2022-10-26 17:52:02 +0530

    added router and navigation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import Directory from './components/directory/directory.component';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats',
-      imageUrl:
-        'https://i.postimg.cc/W3jnCSb4/joshua-coleman-y-VRLC75-Ma8-unsplash.jpg',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-      imageUrl:
-        'https://i.postimg.cc/KjVktv1M/markus-spiske-jddw6i-A31s-E-unsplash.jpg',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-      imageUrl: 'https://i.postimg.cc/rm5gs3zc/school-aden-gca6783ba0-640.jpg',
-    },
-    {
-      id: 4,
-      title: 'Men',
-      imageUrl: 'https://i.postimg.cc/MpRYdgbB/rayul-M6gy9o-Hg-II-unsplash.jpg',
-    },
-    {
-      id: 5,
-      title: 'Women',
-      imageUrl:
-        'https://i.postimg.cc/Bvjm9NF5/averie-woodard-4nulm-JUYFo-unsplash.jpg',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+const Shop = () => {
+  return <h2>Hello shop here</h2>;
 };
+
+const App = () => (
+  <Routes>
+    <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />} />
+      <Route path='/shop' element={<Shop />} />
+    </Route>
+  </Routes>
+);
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,42 @@
+import Directory from '../../components/directory/directory.component';
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl:
+        'https://i.postimg.cc/W3jnCSb4/joshua-coleman-y-VRLC75-Ma8-unsplash.jpg',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl:
+        'https://i.postimg.cc/KjVktv1M/markus-spiske-jddw6i-A31s-E-unsplash.jpg',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.postimg.cc/rm5gs3zc/school-aden-gca6783ba0-640.jpg',
+    },
+    {
+      id: 4,
+      title: 'Men',
+      imageUrl: 'https://i.postimg.cc/MpRYdgbB/rayul-M6gy9o-Hg-II-unsplash.jpg',
+    },
+    {
+      id: 5,
+      title: 'Women',
+      imageUrl:
+        'https://i.postimg.cc/Bvjm9NF5/averie-woodard-4nulm-JUYFo-unsplash.jpg',
+    },
+  ];
+
+  return (
+    <div>
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default Home;
