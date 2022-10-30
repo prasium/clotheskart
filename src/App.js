76022c995db0d546bee6df596c3eb9ecaf690e21
Prasun Verma <prasunverma12@gmail.com>
2022-10-30 14:47:09 +0530

    added shops page and cart component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
-
-const Shop = () => {
-  return <h2>Hello shop here</h2>;
-};
+import Shop from './routes/shop/shop.component';
 
 const App = () => (
   <Routes>
